aa35ca6ec5ed29f50a83d36803265ca61984f046
donank <11donank22@gmail.com>
2017-03-10 21:38:32 +0530

    Solving problem 3

---
+++ problem3.cpp
@@ -0,0 +1,26 @@
+/*
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+*/
+
+#include <iostream>
+
+main()
+{
+	long num = 600851475143, x;
+	for(i = 0; i< num; i++)
+	{
+		if (num % i == 0)
+		{
+			while(j <= 9)
+			{
+				if(i % j == 0)
+					break;
+				
+			}
+		}
+	}
+}
